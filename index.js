1191a8929d984786fec8f4096cef4bdf8f152f95
tanzer-raihan <tanzerraihan@gmail.com>
2021-11-08 21:53:59 +0600

    fourth commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const { MongoClient } = require('mongodb');
 const ObjectId=require('mongodb').ObjectId;
 const app = express();
-const port = 5000;
+const port =process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 require('dotenv').config();
